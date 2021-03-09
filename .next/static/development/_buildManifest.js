ac971fe3944c30d352c3da284bcc6e00846f3374
dilane3 <komboudilane125@gmail.com>
2021-03-09 14:29:36 +0100

    update

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/auth/login":["static\u002Fchunks\u002Fpages\u002Fauth\u002Flogin.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fauth\u002Flogin"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
 self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/auth/login":["static\u002Fchunks\u002Fpages\u002Fauth\u002Flogin.js"],"/auth/test":["static\u002Fchunks\u002Fpages\u002Fauth\u002Ftest.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fauth\u002Flogin","\u002Fauth\u002Ftest"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> c0dbbc06315d16c97b533b8dbde4fff4f5946b62
